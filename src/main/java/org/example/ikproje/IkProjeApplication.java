395c7d91ea5a65c4ad92b993a1c7f182fed7ccfa
User <evrim98@outlook.com>
2024-12-09 10:10:47 +0300

    Initial commit

---
+++ src/main/java/org/example/ikproje/IkProjeApplication.java
@@ -0,0 +1,13 @@
+package org.example.ikproje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IkProjeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IkProjeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ikproje/IkProjeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ikproje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IkProjeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
