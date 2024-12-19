f67571fa5f9a18219b217c725aec07625f56adaf
msacak <msacakk@gmail.com>
2024-12-19 13:57:37 +0300

    CompanyManager ve Personel için profil güncelleneme endpointleri eklendi.

---
+++ src/main/java/org/example/ikproje/dto/request/UpdateCompanyManagerProfileRequestDto.java
@@ -0,0 +1,53 @@
+package org.example.ikproje.dto.request;
+
+import jakarta.validation.constraints.Pattern;
+import org.example.ikproje.entity.enums.EMembershipType;
+import org.example.ikproje.entity.enums.EUserDepartmentType;
+
+import java.time.LocalDate;
+
+public record UpdateCompanyManagerProfileRequestDto(
+
+        //user
+        String token,
+        Long id,
+        String firstName,
+        String lastName,
+        String phone,
+        String email,
+
+        //userDetails
+        Double salary,
+        LocalDate hireDate,
+        @Pattern(regexp = "\\d{11}", message = "TC No 11 haneli olmalıdır ve tamamı rakamlardan oluşmalıdır.")
+        String tcNo,
+        String sgkNo,
+        LocalDate birthDate,
+        EUserDepartmentType departmentType,
+        //userAddress
+        String region,
+        String city,
+        String district,
+        String neighbourhood,
+        String street,
+        String postalCode,
+        String aptNumber,
+        //companyAddress
+        String companyRegion,
+        String companyCity,
+        String companyDistrict,
+        String companyNeighbourhood,
+        String companyStreet,
+        String companyPostalCode,
+        String companyAptNumber,
+        //company
+        String companyName,
+        String companyPhone,
+        LocalDate companyFoundationDate,
+        String companyIndustry
+
+
+
+
+) {
+}
--- src/main/java/org/example/ikproje/service/UserDetailsService.java
+++ src/main/java/org/example/ikproje/service/UserDetailsService.java
@@ -2,6 +2,8 @@ package org.example.ikproje.service;
 
 import lombok.RequiredArgsConstructor;
 import org.example.ikproje.entity.UserDetails;
+import org.example.ikproje.exception.ErrorType;
+import org.example.ikproje.exception.IKProjeException;
 import org.example.ikproje.repository.UserDetailsRepository;
 import org.example.ikproje.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -13,5 +15,9 @@ public class UserDetailsService {
 	
 	public void save(UserDetails userDetails) {
 		userDetailsRepository.save(userDetails);
+	}
+
+	public UserDetails findByUserId(Long id) {
+		return userDetailsRepository.findByUserId(id).orElseThrow(()->new IKProjeException(ErrorType.PAGE_NOT_FOUND));
 	}
 }
