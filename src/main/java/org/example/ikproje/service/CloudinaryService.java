5d9b6b5209ae950fad3ad368b943fc5a2886f4bd
msacak <msacakk@gmail.com>
2025-01-07 12:27:24 +0300

    Utility generator sınıfı güncellendi.

--- src/main/java/org/example/ikproje/service/CloudinaryService.java
+++ src/main/java/org/example/ikproje/service/CloudinaryService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
@@ -18,6 +19,10 @@ public class CloudinaryService {
 	
 	public String uploadFile(MultipartFile file) throws IOException {
 		Map uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.emptyMap());
+		return (String) uploadResult.get("url");
+	}
+	public String uploadNormalFile(File file) throws IOException {
+		Map uploadResult = cloudinary.uploader().upload(file, ObjectUtils.emptyMap());
 		return (String) uploadResult.get("url");
 	}
 }
--- src/main/java/org/example/ikproje/utility/data/GenerateData.java
+++ src/main/java/org/example/ikproje/utility/data/GenerateData.java
@@ -4,8 +4,15 @@ package org.example.ikproje.utility.data;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.example.ikproje.entity.Admin;
+import org.example.ikproje.entity.Company;
+import org.example.ikproje.entity.User;
 import org.example.ikproje.repository.*;
+import org.example.ikproje.service.CloudinaryService;
 import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 
 @Component
@@ -17,10 +24,11 @@ public class GenerateData {
     private final AddressRepository addressRepository;
     private final CompanyRepository companyRepository;
     private final UserDetailsRepository userDetailsRepository;
+    private final CloudinaryService cloudinaryService;
 
 
     @PostConstruct
-    public void generateData(){
+    public void generateData() throws IOException {
         if(adminRepository.count() == 0){
             Admin admin = Admin.builder()
                     .id(99999L)
@@ -31,11 +39,27 @@ public class GenerateData {
         }
 
         if(userRepository.count() == 0){
-            userRepository.saveAll(GenerateUser.generateCompanyManager());
+            List<User> managers = GenerateUser.generateCompanyManager();
+            userRepository.saveAll(managers);
+
+
             userRepository.saveAll(GenerateUser.generateEmployee());
         }
         if(companyRepository.count() == 0){
-            companyRepository.saveAll(GenerateUser.generateCompany());
+            List<Company> companyList = GenerateUser.generateCompany();
+            File file1 = new File("D:/Projects/IKProje/src/main/java/org/example/ikproje/utility/images/koc.jpg");
+            File file2 = new File("D:/Projects/IKProje/src/main/java/org/example/ikproje/utility/images/trump.jpg");
+            File file3 = new File("D:/Projects/IKProje/src/main/java/org/example/ikproje/utility/images/apple.png");
+            File file4 = new File("D:/Projects/IKProje/src/main/java/org/example/ikproje/utility/images/microsoft.png");
+            File file5 = new File("D:/Projects/IKProje/src/main/java/org/example/ikproje/utility/images/amazon.png");
+            companyList.get(0).setLogo(cloudinaryService.uploadNormalFile(file1));
+            companyList.get(1).setLogo(cloudinaryService.uploadNormalFile(file2));
+            companyList.get(2).setLogo(cloudinaryService.uploadNormalFile(file3));
+            companyList.get(3).setLogo(cloudinaryService.uploadNormalFile(file4));
+            companyList.get(4).setLogo(cloudinaryService.uploadNormalFile(file5));
+            companyRepository.saveAll(companyList);
+
+
         }
         if(addressRepository.count() == 0){
             addressRepository.saveAll(GenerateUser.generateAddress());
